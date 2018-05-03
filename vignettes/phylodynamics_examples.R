bf2d9f1f87328c324a7295d0deb0d89cca90a7c3
Jim Faulkner <jfaulknr@uw.edu>
2018-05-03 16:51:40 -0700

    updated phylodynamics vignette to final form

--- vignettes/phylodynamics_examples.R
+++ vignettes/phylodynamics_examples.R
@@ -4,6 +4,9 @@
 
 ## ----eval=FALSE----------------------------------------------------------
 #  
+#  # set random number seed for reproducibility
+#  set.seed(5)
+#  
 #  # Generate simulated data
 #  nsamp <- 500   #number of samples total
 #  nstart <- 50   #number of samples at time zero
@@ -31,6 +34,13 @@
 #  plot(sgrid$midpts, truetraj, xlim=range(rev(sgrid$midpts)), ylim=c(0, 1.2), type="l", col="red",
 #       xlab="Time before present", ylab="Effective population size")
 #  
+#  png(file="vignettes/figure/phylo_bottleneck_traj.png", width=1000, height=600)
+#  par(font.axis=2, font.lab=2)
+#  plot(sgrid$midpts, truetraj, xlim=range(rev(sgrid$midpts)), ylim=c(0, 1.2), type="l", col="red",
+#       xlab="Time before present", ylab="Effective population size", lwd=3)
+#  dev.off()
+#  
+#  
 
 ## ----eval=FALSE----------------------------------------------------------
 #  
@@ -52,7 +62,7 @@
 #  fit.G <- spmrf(prior="normal", likelihood="coalescent", order=1, data=cdat, par=pars.G,
 #  					chains=nchain, warmup=nburn, thin=nthin, iter=niter, control=list(adapt_delta=0.98, max_treedepth=12), zeta=zeta)
 #  fit.H <- spmrf(prior="horseshoe", likelihood="coalescent", order=1, data=cdat, par=pars.H,
-#  					chains=nchain, warmup=nburn, thin=nthin, iter=niter, control=list(adapt_delta=0.98, max_treedepth=12), zeta=zeta)
+#  					chains=nchain, warmup=nburn, thin=nthin, iter=niter, control=list(adapt_delta=0.995, max_treedepth=12), zeta=zeta)
 #  
 #  # Extract posterior draws
 #  pout.G <- as.array(fit.G)
@@ -64,6 +74,40 @@
 #  
 
 ## ----eval=FALSE----------------------------------------------------------
+#  
+#  # Print parameter summaries
+#  print(fit.G, pars=pars.G)
+#  print(fit.H, pars=pars.H)
+#  
+#  # Some example trace plots for the horseshoe model
+#  plot_trace(pout.H, "theta[10]", pscale="original", stack=TRUE, colset="black")
+#  plot_trace(pout.H, "tau[10]", pscale="log", stack=TRUE, colset="black")
+#  plot_trace(pout.H, "gam", pscale="log", stack=TRUE, colset="black")
+#  
+
+## ----eval=FALSE----------------------------------------------------------
+#  
+#  xrng <- rev(range(sgrid$midpts))
+#  yrng <- c(0,1.8)
+#  
+#  png(filename='phylo_bottleneck_posterior_plots.png', width=1500, height=600, res=200)
+#    par(mfrow=c(1,2), mar=c(2,1.5,1.5,1), oma=c(2,2,0,0))
+#    plot_trend(theta=th.G, obstype="coalescent", xvar=sgrid$midpts, main="GMRF-1",
+#  		xlab="", ylab="", xlim=xrng, ylim=yrng)
+#    lines(sgrid$midpts, truetraj, lwd=2, lty=2, col="red")
+#    plot_trend(theta=th.H, obstype="coalescent", xvar=sgrid$midpts, main="HSMRF-1",
+#  		xlab="", ylab="", xlim=xrng, ylim=yrng)
+#    lines(sgrid$midpts, truetraj, lwd=2, lty=2, col="red")
+#   legend(x="topright", legend=c("Median", "95% BCI", "Truth"), col=c("blue","lightblue", "red"), lwd=3, lty=c(1,1,2), bty="n", cex=0.8)
+#   mtext(side=1, outer=T, line=1, text="Time before present", font=2, cex=0.8)
+#   mtext(side=2, outer=T, line=1, text="Effective population size", font=2, cex=0.8)
+#  dev.off()
+#  
+#  
+
+## ----eval=FALSE----------------------------------------------------------
+#  
+#  # load the hcv data
 #  
 #  
 #  
